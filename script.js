e29941170d88f85fb20ac9357801f4ae2d72ad0d
faheemyaqoob <162944444+faheemyaqob@users.noreply.github.com>
2024-09-25 23:51:29 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Script to handle form submission or other interactive elements
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert("Message sent!");
+});
